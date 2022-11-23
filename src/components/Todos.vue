e2bfe812e5e671998fcdef2349f45a9ff0769886
ian gottheim <iangottheim@gmail.com>
2022-11-23 16:14:36 -0500

    youtube course

---
+++ src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>     
+    <div v-for="todo in todos" v-bind:key="todo.id" todo_prop.sync="todo">
+        <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+    </div>
+    </div>
+</template>
+
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: "TodoList",
+    components:{
+        TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
